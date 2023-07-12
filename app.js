5be7a7d1f90cdf03160cd82be5a858c58c5dc49b
Domisiaxa <domisiaxa@gmail.com>
2023-07-13 01:04:49 +0200

    .

--- app.js
+++ app.js
@@ -3,9 +3,7 @@ const logger = require("morgan");
 const cors = require("cors");
 const mongoose = require("mongoose");
 mongoose.Promise = global.Promise;
-
 const path = require("path");
-
 
 const contactsRouter = require("./routes/api/contacts");
 const usersRouter = require("./routes/api/users");
@@ -20,9 +18,7 @@ app.use(express.json());
 
 require("./auth/config/config-passport");
 
-
 app.use("/avatars", express.static(path.join(__dirname, "public", "avatars")));
-
 
 app.use("/contacts", contactsRouter);
 app.use("/users", usersRouter);
